0efe8e3b8c6d88c0c35620a6829806c363cbde12
Leon Alfahrezi <leonalfahrezi@gmail.com>
2022-09-14 17:33:14 +0800

    tambah fitur a

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -3,4 +3,10 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static int fiturA() {
+        // do something to A
+        return 0;
+    }
+
 }
