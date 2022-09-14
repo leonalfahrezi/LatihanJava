3f7c04690c18bd009faa2b239a3d1db9c3ecf39a
Leon Alfahrezi <leonalfahrezi@gmail.com>
2022-09-14 17:50:42 +0800

    tambah buat resolve coflict

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -3,6 +3,12 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static int fiturA() {
+        // do something to A
+        return 0;
+    }
+
     public static String fiturB() {
         return "fitur b";
     }
